872401c10fd07319a64f24c4112a14b2ad29edba
sinjwu <harbirdouk@gmail.com>
2025-06-17 09:04:08 +0900

    250615 review 01-2 (revised 0617 8:59)

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("배열 요소 출력");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers[" + i + "] = "  + numbers[i]);
+        }
+    }
+}
---
+++ src/Overloading_ex.java
@@ -0,0 +1,13 @@
+public class Overloading_ex {
+    public static void greet() {
+        System.out.println("Hello, World!");
+    }
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        greet("Alice");
+    }
+}
